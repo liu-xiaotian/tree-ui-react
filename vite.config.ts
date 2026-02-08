88c724dc0c8ddc95d525684190e890b9cb76829e
tt <xiaotian3303@outlook.com>
2026-02-09 00:16:04 +0800

    初始化文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path = require("node:path");
+import path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
